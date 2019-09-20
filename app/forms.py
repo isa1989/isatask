b75fb1acd0243f6edac26a63144b60a015108f4e
isa1989 <isaramazanov89@gmail.com>
2019-09-21 04:58:54 +0500

    isa_task

--- app/forms.py
+++ app/forms.py
@@ -8,10 +8,11 @@ class TaskForm(forms.ModelForm):
         super(TaskForm, self).__init__(*args,**kwargs)
         for field in self.fields:
             self.fields[field].widget.attrs={'class':'form-control'}
+            self.fields['end_time'].widget.attrs['placeholder'] =  '2019-01-01 00:00:00'
 
     class Meta:
         model = Task
-        fields = ['user', 'other_user', 'comment_user', 'name', 'description', 'end_time']
+        fields = ['other_user', 'comment_user', 'name', 'description', 'end_time']
 
 
 class CommentForm(forms.ModelForm):
@@ -23,5 +24,5 @@ class CommentForm(forms.ModelForm):
 
     class Meta:
         model = Comment
-        fields = ['user','description']
+        fields = ['description']
 
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse, HttpResponseRedirect, reverse, get_object_or_404
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect, reverse, get_object_or_404,redirect
 from django.core.exceptions import ObjectDoesNotExist
 from .forms import TaskForm, CommentForm
 from .models import Task, Comment
@@ -15,7 +15,9 @@ def create(request):
     if request.method == 'POST':
         create_form = TaskForm(request.POST)
         if create_form.is_valid():
-            created_form = create_form.save(commit=True)
+            created_form = create_form.save(commit=False)
+            created_form.user = request.user
+            created_form.save()
             return HttpResponseRedirect(reverse('app:detail', kwargs={'pk':created_form.id}))
     return render(request,'post_create.html', context={'form':create_form})
 
@@ -33,18 +35,13 @@ def list(request):
 
 @login_required(login_url='/users/login/')
 def detail(request, pk):
-    post = get_object_or_404(Task, pk=pk)
-    list = Task.objects.filter(user=request.user)
-    listt = Task.objects.all()
-    if request.user == post.user:
-        form = Task.objects.get(pk=pk)
-
+    #post = get_object_or_404(Task, pk=pk)
+    #list = Task.objects.filter(user=request.user)
+    #listt = Task.objects.all()
     try:
-        request.user == post.other_user
         form = Task.objects.get(pk=pk)
-
     except ObjectDoesNotExist:
-        raise Http404
+        return redirect('app:list')
 
 
 
@@ -56,18 +53,26 @@ def detail(request, pk):
         comment.comm = form
         comment.user = request.user
         comment.save()
-        comment_form = CommentForm()
-    return render(request, 'post_detail.html', context={'post':post, 'form':form, 'list':list, 'listt':listt, 'comment_form':comment_form,})
+        #comment_form = CommentForm()
+        return redirect('app:detail', pk)
+    else:
+        return render(request, 'post_detail.html', context={'form':form,  'comment_form':comment_form,})
 
 @login_required(login_url='/users/login/')
 def update(request, pk):
     task = get_object_or_404(Task, pk=pk)
-    form = TaskForm(data=request.POST or None, instance=task)
-    if request.method == 'POST':
+
+    if request.user == task.user:
+        form = TaskForm(request.POST or None, instance=task)
+        request.method == 'POST'
         if form.is_valid():
             form.save(commit=True)
-        return HttpResponseRedirect(reverse('app:detail', kwargs={'pk':form.instance.id}))
+            return HttpResponseRedirect(reverse('app:detail', kwargs={'pk': form.instance.id}))
+    else:
+        return HttpResponse('sehife tapilmadi')
+
     return render(request,'post_update.html',context={'form':form})
+
 
 
 @login_required(login_url='/users/login/')
@@ -78,16 +83,17 @@ def delete(request, pk):
     post.delete()
     return HttpResponseRedirect(reverse('app:list'))
 
+
+
+
 @login_required(login_url='/users/login/')
-
-
 def comment_delete(request, pk):
     post = get_object_or_404(Comment, pk=pk)
 
     request.user == post.user
 
     post.delete()
-    return HttpResponseRedirect(reverse('app:list'))
+    return redirect('app:list')
 
 
 @login_required(login_url='/users/login/')
@@ -96,8 +102,10 @@ def comment_update(request, pk):
     form = CommentForm(data=request.POST or None, instance=task)
     if request.method == 'POST':
         if form.is_valid():
-            form.save(commit=True)
-        return HttpResponseRedirect(reverse('app:detail', kwargs={'pk':form.instance.id}))
+            updated_comment = form.save(commit=False)
+            updated_comment.user = request.user
+            updated_comment.save()
+        return redirect('app:list')
     return render(request,'comment_update.html',context={'form':form})
 
 
